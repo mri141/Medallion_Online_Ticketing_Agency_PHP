6024a203af7a1dbfccf229b803a412b68e4ec826
mri141 <rashedcse35@gmail.com>
2023-05-05 02:18:51 +0600

    Initial Commit by Rashed

---
+++ index.php
@@ -0,0 +1,476 @@
+﻿<!DOCTYPE html>
+
+
+<head>
+	
+<meta charset="utf-8">
+	
+<meta name="viewport" content="width=devidev-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+	
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Medallion Online Ticketing</title>
+	
+	
+<!-- [ FONT-AWESOME ICON ] 
+        
+=========================================================================================================================-->
+	
+<link rel="stylesheet" type="text/css" href="library/font-awesome-4.3.0/css/font-awesome.min.css">
+
+	
+<!-- [ PLUGIN STYLESHEET ]
+        
+=========================================================================================================================-->
+	
+<link rel="shortcut icon" type="image/x-icon" href="images/icon.png">
+	
+<link rel="stylesheet" type="text/css" href="css/animate.css">
+	
+<link rel="stylesheet" type="text/css" href="css/owl.carousel.css">
+        
+<link rel="stylesheet" type="text/css" href="css/owl.theme.css">
+	
+<link rel="stylesheet" type="text/css" href="css/magnific-popup.css">
+	
+<!-- [ Boot STYLESHEET ]
+        
+=========================================================================================================================-->
+	
+<link rel="stylesheet" type="text/css" href="library/bootstrap/css/bootstrap-theme.min.css">
+	
+<link rel="stylesheet" type="text/css" href="library/bootstrap/css/bootstrap.css">
+       
+<!-- [ DEFAULT STYLESHEET ] 
+        
+=========================================================================================================================-->
+	
+<link rel="stylesheet" type="text/css" href="css/style.css">
+        
+<link rel="stylesheet" type="text/css" href="css/responsive.css">
+	
+<link rel="stylesheet" type="text/css" href="css/color/rose.css">
+        
+
+</head>
+<body >
+
+<!-- [ LOADERs ]
+
+================================================================================================================================-->
+	
+    <div class="preloader">
+    
+<div class="loader theme_background_color">
+        
+<span></span>
+      
+    
+</div>
+</div>
+<!-- [ /PRELOADER ]
+
+=============================================================================================================================-->
+
+<!-- [WRAPPER ]
+
+=============================================================================================================================-->
+
+<div class="wrapper">
+  
+<!-- [NAV]
+ 
+============================================================================================================================-->    
+   
+<!-- Navigation
+    ==========================================-->
+    
+<nav  class=" nim-menu navbar navbar-default navbar-fixed-top">
+      
+<div class="container">
+        
+<!-- Brand and toggle get grouped for better mobile display -->
+        
+<div class="navbar-header">
+          
+<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+            
+<span class="sr-only">Toggle navigation</span>
+            
+<span class="icon-bar"></span>
+            
+<span class="icon-bar"></span>
+            
+<span class="icon-bar"></span>
+          
+</button>
+            
+<a class="navbar-brand" href="index.html">M<span class="themecolor">e</span>dallion</a>
+        
+</div>
+
+        
+<!-- Collect the nav links, forms, and other content for toggling -->
+        
+<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          
+<ul class="nav navbar-nav navbar-right">
+            
+<li><a href="#home" class="page-scroll"><h3>Home</h3></a></li>
+            
+<li><a href="#two" class="page-scroll"><h3>About</h3></a></li>
+                                   
+<li><a href="#one" class="page-scroll"><h3>Inspiration</h3></a></li>
+                                 
+<li><a href="#three" class="page-scroll"><h3>Services</h3></a></li>
+            
+<li><a href="#four" class="page-scroll"><h3>Contact</h3></a></li>
+
+<li><a href="admin/" class="page-scroll"><h3>Login</h3></a></li>
+          
+</ul>
+        
+</div>
+<!-- /.navbar-collapse -->
+      
+</div><!-- /.container-fluid -->
+    
+</nav>
+
+
+   
+<!-- [/NAV]
+ 
+============================================================================================================================--> 
+    
+   
+<!-- [/MAIN-HEADING]
+ 
+============================================================================================================================--> 
+   
+<section class="main-heading" id="home">
+       
+<div class="overlay">
+           
+<div class="container">
+               
+<div class="row">
+                   
+<div class="main-heading-content col-md-12 col-sm-12 text-center">
+        
+<h1 class="main-heading-title"><span class="main-element themecolor" data-elements=" Online Ticket, Online Ticket , Online Ticket "></span></h1>
+ 
+<h1 class="main-heading-title"><span class="main-element themecolor" data-elements=" Ticket Reservation, Ticket Reservation, Ticket Reservation"></span></h1>
+       
+<p class="main-heading-text"></p>
+        
+<div class="btn-bar">
+          
+<a href="reserved.php" class="btn btn-custom theme_background_color">Reserved Now</a>
+                 
+</div>
+      
+</div>
+               
+</div>
+           
+</div>
+       
+</div>  
+      
+   
+</section>
+ 
+<section class="main-heading" id="home">
+       
+<div class="overlay">
+           
+<div class="container">
+               
+<div class="row">
+                   
+<div class="main-heading-content col-md-12 col-sm-12 text-center">
+        
+<h1 class="main-heading-title">We are Creative</h1>
+        
+<p class="main-heading-text">Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore,
+<br/>cum soluta nobis est eligendi optio cumque nihil impedit quo facilis</p>
+        
+<div class="btn-bar">
+          
+<a href="#" class="btn btn-custom theme_background_color">Ge Started</a>
+          
+<a href="#" class="btn btn-custom-outline">Contact Us</a>
+        
+</div>
+      
+</div>
+               
+</div>
+           
+</div>
+       
+</div>  
+      
+   
+</section>
+    
+ 
+<!-- [/MAIN-HEADING]
+ 
+============================================================================================================================-->
+ 
+ 
+ 
+<!-- [ABOUT US]
+ 
+============================================================================================================================-->
+ 
+
+         
+         
+         
+     
+</div>
+ </section>
+ 
+ 
+ 
+<!-- [/ABOUTUS]
+ 
+ 
+============================================================================================================================-->
+ 
+ 
+ 
+ 
+ 
+<!-- [INSPIRATION]
+ 
+============================================================================================================================-->
+ 
+<section class="inspiration" id="four">
+     
+<div class="overlay">
+         
+<div class="container">
+             
+<div class="row">
+                
+ <article class="col-md-12 text-center">
+           
+ <div class="intermediate-container">
+             
+ <div class="subheading">
+                 
+ <h4>Are You Ready To <span class="themecolor">Enjoy?</span></h4>
+           
+   </div>
+             
+ <div class="heading">
+              
+  <h2>inspire Our customer here!</h2>
+          
+    </div>
+              
+<div class="">
+              
+  <a class="btn btn-custom-outline" href="#"><span>get started</span></a>
+        
+      </div>
+        
+    </div>
+       
+   </article>
+       
+      </div>
+    
+    </div>
+    
+ </div>
+ 
+</section>
+ 
+ 
+<!-- [/INSPIRATION]
+ 
+
+============================================================================================================================-->
+ 
+ 
+<!-- [/SERVICES]
+ 
+============================================================================================================================-->
+ 
+ 
+ 
+
+<!-- [/SERVICES]
+ 
+============================================================================================================================-->
+ 
+ 
+ 
+<!-- [CONTACT]
+ 
+============================================================================================================================-->
+ 
+<!--sub-form-->
+<section class="sub-form text-center" id="eight">
+  
+<div class="container">
+    <div class="col-md-12">
+        
+<h3 class="title">Subscribe to My <span class="themecolor"> News letter</span></h3>
+            
+<p class="lead">#</p>
+    
+</div> 
+    
+<div class="row">
+        
+<div class="col-md-3 col-sm-3"></div>
+      
+<div class="col-md-6 center-block col-sm-6 ">
+        
+<form id="mc-form">
+          
+<div class="input-group">
+            
+<input type="email" class="form-control" placeholder="Email Address" required id="mc-email">
+           
+<span class="input-group-btn">
+            
+<button type="submit" class="btn btn-default">SUBSCRIBE <i class="fa fa-envelope"></i> </button>
+            
+</span> </div>
+          
+<label for="mc-email" id="mc-notification"></label>
+       
+ </form>
+      
+</div>
+   
+ </div>
+  
+</div>
+
+</section>
+
+<!--sub-form end--> 
+
+
+ 
+ 
+<!-- [/CONTACT]
+ 
+============================================================================================================================-->
+ 
+ 
+ 
+<!-- [FOOTER]
+ 
+============================================================================================================================-->
+ 
+
+<footer class="site-footer section-spacing text-center " id="eight">
+    
+  
+<div class="container">
+    
+<div class="row">
+      
+<div class="col-md-4">
+        
+<p class="footer-links"><a href="#">Terms of Use</a> <a href="#">Privacy Policy</a></p>
+      
+</div>
+      
+<div class="col-md-4"> <small>&copy; 2017 By Rashed & Darush</small></div>
+      
+<div class="col-md-4"> 
+        
+<!--social-->
+        
+        
+<ul class="social">
+          
+<li><a href="https://twitter.com/" target="_blank"><i class="fa fa-twitter "></i></a></li>
+          
+<li><a href="https://www.facebook.com/" target="_blank"><i class="fa fa-facebook"></i></a></li>
+          
+<li><a href="https://www.youtube.com/" target="_blank"><i class="fa fa-youtube-play"></i></a></li>
+        
+</ul>
+        
+        
+<!--social end--> 
+        
+      
+</div>
+    
+</div>
+  
+</div>
+
+</footer>
+
+ 
+ 
+ 
+<!-- [/FOOTER]
+ 
+============================================================================================================================-->
+ 
+ 
+ 
+
+</div>
+ 
+
+<!-- [ /WRAPPER ]
+
+=============================================================================================================================-->
+
+	
+<!-- [ DEFAULT SCRIPT ] -->
+	
+<script src="library/modernizr.custom.97074.js"></script>
+	
+<script src="library/jquery-1.11.3.min.js"></script>
+        
+<script src="library/bootstrap/js/bootstrap.js"></script>
+	
+<script type="text/javascript" src="js/jquery.easing.1.3.js"></script>	
+	
+<!-- [ PLUGIN SCRIPT ] -->
+        
+<script src="library/vegas/vegas.min.js"></script>
+	
+<script src="js/plugins.js"></script>
+        
+<!-- [ TYPING SCRIPT ] -->
+         
+<script src="js/typed.js"></script>
+         
+<!-- [ COUNT SCRIPT ] -->
+         
+<script src="js/fappear.js"></script>
+       
+<script src="js/jquery.countTo.js"></script>
+	
+<!-- [ SLIDER SCRIPT ] -->  
+        
+<script src="js/owl.carousel.js"></script>
+         
+<script src="js/jquery.magnific-popup.min.js" type="text/javascript"></script>
+        
+<script type="text/javascript" src="js/SmoothScroll.js"></script>
+        
+        
+<!-- [ COMMON SCRIPT ] -->
+	<script src="js/common.js"></script>
+  
+</body>
+
+
+</html>
